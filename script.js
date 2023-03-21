92fe7d8668c2ad56584de06dd4d7b50f3b27246f
Benedictus Kent <benedictuskent123@gmail.com>
2023-03-22 00:10:02 +0800

    finish basic layout

---
+++ script.js
@@ -0,0 +1,26 @@
+const ctx = document.getElementById("myChart");
+
+Chart.defaults.font.size = 16;
+Chart.defaults.font.family = "Helvetica";
+Chart.defaults.color = "#000";
+
+new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+            {
+                label: "# of Votes",
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
+});
