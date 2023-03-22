dce89a6e4cb40a5181df488fe41db3a1346c5b7b
Benedictus Kent <benedictuskent123@gmail.com>
2023-03-22 23:42:07 +0800

    change data

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const getWeekDate = function () {
+    const dates = [];
+    const today = new Date();
+    for (let i = 0; i < 7; i++) {
+        text = `${today.getMonth() + 1}/${today.getDate()}`;
+        dates.splice(0, 0, text);
+        today.setDate(today.getDate() - 1);
+    }
+    return dates;
+};
+
 const ctx = document.getElementById("myChart");
 
 Chart.defaults.font.size = 16;
@@ -7,19 +18,20 @@ Chart.defaults.color = "#000";
 new Chart(ctx, {
     type: "line",
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: getWeekDate(),
         datasets: [
             {
-                label: "# of Votes",
-                data: [12, 19, 3, 5, 2, 3],
-                borderWidth: 1,
+                label: "Weight in kg",
+                data: [78.9, 78.25, 77.65, 78.8, 78.1, 79, 78.3],
+                borderWidth: 2,
+                borderColor: "#3ebc5a",
             },
         ],
     },
     options: {
         scales: {
             y: {
-                beginAtZero: true,
+                beginAtZero: false,
             },
         },
     },
